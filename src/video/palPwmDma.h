c3405666fa646734e11a7dffa1160d51e5189141
bitluni <bitluni@bitluni.net>
2025-09-19 14:06:49 +0200

    reduced memory usage. two lines only

--- src/video/palPwmDma.h
+++ src/video/palPwmDma.h
@@ -15,7 +15,8 @@ const int levelBlack = 8;
 const int levelGrey = 20;
 const int levelWhite = 32;
 
-__attribute__((aligned(4))) uint32_t vram[10][200 - 0];
+__attribute__((aligned(4))) uint32_t vram[2][400 - 0];
+__attribute__((aligned(4))) uint32_t syncVram[2][400 - 0];
 volatile uint16_t frameLines[312];
 
 void setLineSync(uint32_t *line)
@@ -100,38 +101,29 @@ void initVideo()
 
     TMR_PWMInit(High_Level, 0);
 
-    setLineLongLong(vram[8]);
-    setLineShortShort(vram[9]);
-    //setLineLongShort(vram[10]);
+    setLineLongLong(syncVram[0]);
+    setLineShortShort(syncVram[1]);
 
     for(int r = 0; r < 16; r++)
         for(int c = 0; c < 16; c++)
             textBuffer[r][c] = startText[r][c] - 32;
 
-    for(int y = 0; y < 8; y++)
+    for(int y = 0; y < 2; y++)
     {
         setLineSync(vram[y]);
-        for(int x = 0; x < 128; x++)
-        {
-            int ch = textBuffer[0][x >> 3];
-            int bit = x & 7;
-            vram[y][x + pixelsSync + pixelsFront] = levelBlack + (x & 7);
-            if((font8x8[ch][y] >> bit) & 1)
-                vram[y][x + pixelsSync + pixelsFront] = levelWhite;
-        }
     }
 
     int i = 0;
     for(; i < 304; i++)
-        frameLines[i + 8] = (uint32_t)vram[((i >> 1) & 7)];
-    frameLines[i++] = (uint32_t)vram[9];
-    frameLines[i++] = (uint32_t)vram[9];
-    frameLines[i++] = (uint32_t)vram[9];
-    frameLines[i++] = (uint32_t)vram[8];
-    frameLines[i++] = (uint32_t)vram[8];
-    frameLines[i++] = (uint32_t)vram[8] + 400;    //long short
-    frameLines[i++] = (uint32_t)vram[9];
-    frameLines[i++] = (uint32_t)vram[9];
+        frameLines[i + 8] = (uint32_t)vram[((i >> 1) & 1)];
+    frameLines[i++] = (uint32_t)syncVram[1];
+    frameLines[i++] = (uint32_t)syncVram[1];
+    frameLines[i++] = (uint32_t)syncVram[1];
+    frameLines[i++] = (uint32_t)syncVram[0];
+    frameLines[i++] = (uint32_t)syncVram[0];
+    frameLines[i++] = (uint32_t)syncVram[0] + 400;    //long short
+    frameLines[i++] = (uint32_t)syncVram[1];
+    frameLines[i++] = (uint32_t)syncVram[1];
 
     SysTick->CTLR = 0;
     SysTick->CNT = 0;
@@ -173,14 +165,13 @@ void TMR_IRQHandler(void) // TMR0
     uint32_t beg = frameLines[currentLine];
     //fix sync
     while(!SysTick->SR);
-    SysTick->SR = 0;
     R16_TMR_DMA_BEG = beg;
     R16_TMR_DMA_END = beg + 800 - (pixelsReload << 2);
-
     TMR_PWMInit(High_Level, 0);
-
     TMR_PWMEnable();
     TMR_Enable();
+    SysTick->SR = 0;
+
     if(TMR_GetITFlag(TMR_IT_CYC_END))
     {
         TMR_ClearITFlag(TMR_IT_CYC_END);
@@ -191,10 +182,10 @@ void TMR_IRQHandler(void) // TMR0
     {
         if(currentLine < 256)
         {
-            int renderLine = ((currentLine >> 1) + 7) & 127;
+            int renderLine = ((currentLine >> 1) + 3) & 127;
             int r = renderLine >> 3;
             int y = renderLine & 7;
-            uint32_t *p = &(vram[y][pixelsSync + pixelsFront]);
+            uint32_t *p = &(vram[y & 1][pixelsSync + pixelsFront]);
             {
                 for(int x = 0; x < 8 * 16; x++)
                 {
